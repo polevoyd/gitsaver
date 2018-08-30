308a9b7886e91da8a396acbed237abd8d9a88894
Dmitry <manxxeta@gmail.com>
2018-08-30 13:15:33 -0800

    gitsaver

--- src/components/Results.js
+++ src/components/Results.js
@@ -33,7 +33,7 @@ const Results = (props) => {
         // 1 - 1
         // 2 - 13 contributions
         // 3 - 26 contributions
-        // 4 - 29
+        // 4 - 30
         // console.log(startingDay())
 
 
