afaae3006a887e5554f811e3488fb5b55df83e13
Dmitry <manxxeta@gmail.com>
2018-11-21 12:52:22 -0800

    static version

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BoardCell from './BoardCell';
 
 const Board = () => {
-    return(
-        <div>
 
+    const arrayOfCells = new Array(54);
+    return(
+        <div className="board">
+            {/* loop that renders full board  */}
+            {arrayOfCells.map(e => <BoardCell/>)}
+            <BoardCell/>
         </div>
     );
 }
---
+++ src/components/BoardCell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BoardCell = () => {
+    return(
+        <div className='board-cell'></div>
+    );
+}
+
+export default BoardCell;
