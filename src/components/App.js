8228f68084db2fe6ec1185eb3e64083e96897830
Dmitry <manxxeta@gmail.com>
2018-11-21 12:26:16 -0800

    static version

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Welcome from './Welcome';
 
 class App extends React.Component {
   render(){
     return(
       <div>
-        
+        <Welcome/>
       </div>
     );
   }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 const Welcome = () => {
     return(
-        <div>
+        <div className="welcome">
             <h4>
-                
+                Namename
             </h4>
+            <p>
+                text text text text text text text text text text text text text 
+                text text text text text text text text text text text text text
+                text text text text text text text text text text text text text
+                text text text text text text text text text text text text text
+                text text text text text text text text text text text text text
+                
+            </p>
         </div>
     );
 }
+
+export default Welcome;
