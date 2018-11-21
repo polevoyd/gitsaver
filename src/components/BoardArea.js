36ffc2988a6e9bbe16474340f39b01ad45dff371
Dmitry <manxxeta@gmail.com>
2018-11-21 15:34:03 -0800

    static version

--- src/components/BoardArea.js
+++ src/components/BoardArea.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './Board';
+import ColorSwitch from './ColorSwitch';
 
 const BoardArea = () => {
     return(
         <div className="board-area">
             <Board/>
+            <ColorSwitch />
         </div>
     );
 }
---
+++ src/components/ColorSwitch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ColorSwitch = () => {
+    return(
+        <div>
+            <form className="color-switch">
+                <div className="radio">
+    
+                </div>
+                <div className="radio">
+        
+                </div>
+                <div className="radio">
+   
+                </div>
+                <div className="radio">
+
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default ColorSwitch;
