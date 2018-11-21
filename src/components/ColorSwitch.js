1775f0924949f147be17ea04e8d8bb77b8f00f99
Dmitry <manxxeta@gmail.com>
2018-11-21 15:44:13 -0800

    static version

--- src/components/ColorSwitch.js
+++ src/components/ColorSwitch.js
@@ -4,18 +4,11 @@ const ColorSwitch = () => {
     return(
         <div>
             <form className="color-switch">
-                <div className="radio">
-    
-                </div>
-                <div className="radio">
-        
-                </div>
-                <div className="radio">
-   
-                </div>
-                <div className="radio">
-
-                </div>
+                <div className="radio" id="radio1"></div>
+                <div className="radio" id="radio2"></div>
+                <div className="radio" id="radio3"></div>
+                <div className="radio" id="radio4"></div>
+                <div className="radio" id="radio5"></div>
             </form>
         </div>
     );
