ec44bbdf9c09ebc4c2264e30c8073e393934da2e
Dmitry <manxxeta@gmail.com>
2018-11-22 09:52:10 -0800

    hooks

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,22 @@ import BoardCell from './BoardCell';
 
 const Board = () => {
 
-    const arrayOfCells = new Array(54*7).fill(0).map((e,i) => <BoardCell key={i} index={i}></BoardCell>)
+    const arrayOfCells = new Array(52*7).fill(0).map((e,i) => <BoardCell key={i} index={i}></BoardCell>)
+    
+    const year = new Date().getFullYear()-1;
+
+    const start = new Date().setFullYear(year)
+
+    const g = new Date(start)
+
+
+
+
+
+
+
+    console.log(g)
+    
     return(
         <div className="board">
             <ul className="squares">
