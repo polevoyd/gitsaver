f68ed8a251d47a34892c03d45a53e2dad14c8e94
Dmitry <manxxeta@gmail.com>
2018-11-21 19:15:41 -0800

    hooks

--- src/components/ColorSwitch.js
+++ src/components/ColorSwitch.js
@@ -3,14 +3,15 @@ import { useState } from 'react';
 
 const ColorSwitch = () => {
     const [color, setColor] = useState('');
+    
     return(
         <div>
             <form className="color-switch">
                 <div className="color" id="color1" onClick={e => setColor('#196127')}></div>
-                <div className="color" id="color2" onClick={e => setColor('#196127')}></div>
-                <div className="color" id="color3" onClick={e => setColor('#196127')}></div>
-                <div className="color" id="color4" onClick={e => setColor('#196127')}></div>
-                <div className="color" id="color5" onClick={e => setColor('#196127')}></div>
+                <div className="color" id="color2" onClick={e => setColor('#239a3b')}></div>
+                <div className="color" id="color3" onClick={e => setColor('#7bc96f')}></div>
+                <div className="color" id="color4" onClick={e => setColor('#c6e48b')}></div>
+                <div className="color" id="color5" onClick={e => setColor('#ebedf0')}></div>
             </form>
         </div>
     );
