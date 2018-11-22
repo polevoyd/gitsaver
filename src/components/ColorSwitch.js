c2a673a2d1c4e7d1262e8ed4a99104a91715ecc3
Dmitry <manxxeta@gmail.com>
2018-11-21 19:20:52 -0800

    hooks

--- src/components/ColorSwitch.js
+++ src/components/ColorSwitch.js
@@ -7,14 +7,22 @@ const ColorSwitch = () => {
     return(
         <div>
             <form className="color-switch">
-                <div className="color" id="color1" onClick={e => setColor('#196127')}></div>
-                <div className="color" id="color2" onClick={e => setColor('#239a3b')}></div>
-                <div className="color" id="color3" onClick={e => setColor('#7bc96f')}></div>
-                <div className="color" id="color4" onClick={e => setColor('#c6e48b')}></div>
-                <div className="color" id="color5" onClick={e => setColor('#ebedf0')}></div>
+                <div className="color" id="color1"></div>
+                <div className="color" id="color2"></div>
+                <div className="color" id="color3"></div>
+                <div className="color" id="color4"></div>
+                <div className="color" id="color5"></div>
             </form>
         </div>
     );
 }
 
 export default ColorSwitch;
+
+/*
+onClick={e => setColor('#196127')}
+onClick={e => setColor('#239a3b')}
+onClick={e => setColor('#7bc96f')}
+onClick={e => setColor('#c6e48b')}
+onClick={e => setColor('#ebedf0')}
+ */
