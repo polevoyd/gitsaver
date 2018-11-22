6d77395c560f5195d0631ce594cbedeb5af4a7c2
Dmitry <manxxeta@gmail.com>
2018-11-22 11:55:59 -0800

    hooks

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,17 @@ const App = () => {
   const currentBoard = new Array(52*7).fill(0);
 
   const [color, setColor] = useState(currentColor)
-  const [board, setCell] = useState(currentBoard);
+  const [board, setBoard] = useState(currentBoard);
 
   const updateBoard = (index, value) => {
 
     
     let copy = board;
     copy[index] = value;
+    
     console.log(copy);
-    setCell(copy);
+    setBoard(copy);
+
   }
 
     return(
