9ee20e48e1a9ad142c7ab735ee81d89b06898582
Dmitry <manxxeta@gmail.com>
2018-11-23 15:44:49 -0800

    W

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,12 +5,13 @@ const Instructions = () => {
         <div className="instructions">
             <h5>Instructions</h5>
             <ol>
-                <li>Pick a color and start drawing</li>
-                <li>After you done, download script file</li>
-                <li>Make it executable by running this command in terminal from same folder</li>
-                <li>Create an empty new repository and copy link to it *</li>
-                <li>Run file and follow instructions (you'll need to paste link)</li>
+                <li>After you finished drawing, click <button>Download Script</button> to download script file</li>
+                <li>Open terminal in same folder and run <span>chmod u+x *filename*</span> to make it executable</li>
+                <li>Create an empty repository and copy link to it *</li>
+                <li>Enter <span>sh *filename*</span> in terminal to run script. Paste link to repo when script ask for it</li>
+                <li>Yay!</li>
             </ol>
+            <a href="http://www.polevoy.in/" target="_blank">made by polevoy</a>
         </div>
     );
 }
