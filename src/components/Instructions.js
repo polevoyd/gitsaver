d1c2009967c28fcc5ebb4976cab84786f6357a64
Dmitry <manxxeta@gmail.com>
2018-11-23 14:46:45 -0800

    W

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Instructions = () => {
     return(
-        <div>
+        <div className="instructions">
             <h5>Instructions</h5>
             <ul>
                 <li>Pick a color and start drawing</li>
                 <li>After you done, download script file</li>
-                <li>Make it executable by running this command from terminal</li>
+                <li>Make it executable by running this command in terminal from same folder</li>
                 <li>Create an empty new repository and copy link to it *</li>
                 <li>Run file and follow instructions (you'll need to paste link)</li>
             </ul>
