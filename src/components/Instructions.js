9e3bef0dd9382e98357087b5af13ad521c3e675d
Dmitry <manxxeta@gmail.com>
2018-11-23 16:56:24 -0800

    W

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -7,7 +7,7 @@ const Instructions = () => {
             <ol>
                 <li>After you finished drawing, click <button>Download Script</button> to download script file</li>
                 <li>Open terminal in same folder and run <span>chmod u+x *filename*</span> to make it executable</li>
-                <li>Create an empty repository and copy link to it *</li>
+                <li>Create an empty repository and copy link to it</li>
                 <li>Enter <span>sh *filename*</span> in terminal to run script. Paste link to repo when script ask for it</li>
                 <li>Yay!</li>
             </ol>
