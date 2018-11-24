af802e9f8509864709351f54c10df1fec4c9c06c
Dmitry <manxxeta@gmail.com>
2018-11-24 15:45:41 -0800

    W

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const Welcome = (props) => {
     const buttonColor = (props) => {
+
+
         return props.faqIsOn ? {backgroundColor: '#d10000'} : {backgroundColor: '#89c403'};
     }
 
@@ -12,9 +14,9 @@ const Welcome = (props) => {
                 A screensaver for your github calendar.
             </p>
             <button 
-                onClick={props.switchFaq} className="button-faq" 
-                style={buttonColor(props)}
-            >
+                onClick={props.switchFaq}
+                className="button-faq" 
+                style={buttonColor(props)}>
             FAQ
             </button>
         </div>
