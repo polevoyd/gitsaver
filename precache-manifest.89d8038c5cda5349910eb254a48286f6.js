898ccdd004999549ce81ee669bf36eb12512ce06
Dmitry <manxxeta@gmail.com>
2018-11-24 16:03:14 -0800

    Updates

---
+++ precache-manifest.89d8038c5cda5349910eb254a48286f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a42abaff9f80876ec23d",
+    "url": "/gitsaver/static/js/runtime~main.a42abaff.js"
+  },
+  {
+    "revision": "198b52c65559fe03db9d",
+    "url": "/gitsaver/static/js/main.198b52c6.chunk.js"
+  },
+  {
+    "revision": "f441e4f58c8c60719dbc",
+    "url": "/gitsaver/static/js/1.f441e4f5.chunk.js"
+  },
+  {
+    "revision": "198b52c65559fe03db9d",
+    "url": "/gitsaver/static/css/main.ef7309d8.chunk.css"
+  },
+  {
+    "revision": "9d00ee659a5848e1909e06f6c6a3cb90",
+    "url": "/gitsaver/index.html"
+  }
+];
