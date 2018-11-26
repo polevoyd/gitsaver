c1025bb7dbb81348c330e71b829fccb9101a821c
Dmitry <manxxeta@gmail.com>
2018-11-26 14:59:59 -0800

    W

--- src/components/ExamplesBoard.js
+++ src/components/ExamplesBoard.js
@@ -3,7 +3,11 @@ import React from 'react';
 const ExamplesBoard = (props) => {
     return(
         <div className="examples-board">
-            Hello! Iam a examples boards
+            <div className="example-preview"></div>
+            <div className="example-preview"></div>
+            <div className="example-preview"></div>
+            <div className="example-preview"></div>
+            <div className="example-preview"></div>
         </div>
     );
 }
