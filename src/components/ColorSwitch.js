f60117c6655e16e1f16c85ccbaeed39ca9368e8a
Dmitry <manxxeta@gmail.com>
2018-12-30 10:49:59 -0800

    Fixing small bugs

--- src/components/ColorSwitch.js
+++ src/components/ColorSwitch.js
@@ -1,29 +1,31 @@
 import React from 'react';
 
 const ColorSwitch = (props) => {
-   const colorOrderNumber = color => {
-       switch (color) {
-           case '#ebedf0': return 5;
-           case '#c6e48b': return 4;
-           case '#7bc96f': return 3;
-           case '#239a3b': return 2;
-           case '#196127': return 1;
-           default:
-               break;
-       }
+   const colorOrderNumber = (color, num) => {
+       
+        //    case '#ebedf0': return 5;
+        //    case '#c6e48b': return 4;
+        //    case '#7bc96f': return 3;
+        //    case '#239a3b': return 2;
+        //    case '#196127': return 1;
+        //    default:
+        //        break;
+       
+
+       
    }
 
-   const getIdNumOfButton = e => console.log(e.target)
+   const getIdNumOfButton = e => e.target.id[e.target.id.length-1]
 
 
    const orangeFrame = { outline: '1px solid orange' }
 
 
-   console.log(colorOrderNumber(props.color))
+//    console.log(colorOrderNumber(props.color))
     return(
         <div className="color-switch-area">
             <form className="color-switch">
-                <div className="color" id="color1" onClick={e => props.setColor('#196127')}></div>
+                <div className="color" id="color1" onClick={e => {props.setColor('#196127'); console.log(getIdNumOfButton(e))}}></div>
                 <div className="color" id="color2" onClick={e => props.setColor('#239a3b')}></div>
                 <div className="color" id="color3" onClick={e => props.setColor('#7bc96f')}></div>
                 <div className="color" id="color4" onClick={e => props.setColor('#c6e48b')}></div>
