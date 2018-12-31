6f163e93373fb865a4b96e6e24b58edc46fb0fb7
Dmitry <manxxeta@gmail.com>
2018-12-31 12:37:43 -0800

    Remodeling

--- src/components/ColorSwitch.js
+++ src/components/ColorSwitch.js
@@ -1,35 +1,25 @@
 import React from 'react';
 
 const ColorSwitch = (props) => {
-   const colorOrderNumber = (color, num) => {
-       
-        //    case '#ebedf0': return 5;
-        //    case '#c6e48b': return 4;
-        //    case '#7bc96f': return 3;
-        //    case '#239a3b': return 2;
-        //    case '#196127': return 1;
-        //    default:
-        //        break;
-       
-
-       
+    
+   const setOrangeOutline = (color) => {
+    // clean previous outline and set a new one
+    const cleanOutlines = Array.from(document.getElementsByClassName('color')).forEach(e => e.style.outline = 'none')
+    if (color === '#ebedf0') document.getElementById('color5').style.outline = '1px solid orange'
+    if (color === '#c6e48b') document.getElementById('color4').style.outline = '1px solid orange'
+    if (color === '#7bc96f') document.getElementById('color3').style.outline = '1px solid orange'
+    if (color === '#239a3b') document.getElementById('color2').style.outline = '1px solid orange'
+    if (color === '#196127') document.getElementById('color1').style.outline = '1px solid orange' 
    }
 
-   const getIdNumOfButton = e => e.target.id[e.target.id.length-1]
-
-
-   const orangeFrame = { outline: '1px solid orange' }
-
-
-//    console.log(colorOrderNumber(props.color))
     return(
         <div className="color-switch-area">
             <form className="color-switch">
-                <div className="color" id="color1" onClick={e => {props.setColor('#196127'); console.log(getIdNumOfButton(e))}}></div>
-                <div className="color" id="color2" onClick={e => props.setColor('#239a3b')}></div>
-                <div className="color" id="color3" onClick={e => props.setColor('#7bc96f')}></div>
-                <div className="color" id="color4" onClick={e => props.setColor('#c6e48b')}></div>
-                <div className="color" id="color5" onClick={e => props.setColor('#ebedf0')}></div>
+                <div className="color" id="color1" onClick={e => {props.setColor('#196127'); setOrangeOutline('#196127')}}></div>
+                <div className="color" id="color2" onClick={e => {props.setColor('#239a3b'); setOrangeOutline('#239a3b')}}></div>
+                <div className="color" id="color3" onClick={e => {props.setColor('#7bc96f'); setOrangeOutline('#7bc96f')}}></div>
+                <div className="color" id="color4" onClick={e => {props.setColor('#c6e48b'); setOrangeOutline('#c6e48b')}}></div>
+                <div className="color" id="color5" onClick={e => {props.setColor('#ebedf0'); setOrangeOutline('#ebedf0')}}></div>
             </form>
         </div>
     );
