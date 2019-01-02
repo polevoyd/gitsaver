5450d641c00b6d87eba751d0091942a71cadc58d
Dmitry <manxxeta@gmail.com>
2019-01-02 13:41:44 -0800

    Remodeling

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,14 +4,15 @@ const Welcome = (props) => {
     const buttonColor = (props) => {
         return props.faqIsOn ? {backgroundColor: '#d10000'} : {backgroundColor: '#89c403'};
     }
-
+    
     return(
-        <div className="welcome">
+        <div 
+        className="welcome">
             <h4 className="changing-colors">gitsaver</h4> 
             <p>
                 A screensaver for your github calendar.
             </p>
-            <button
+            <button 
                 onClick={props.switchFaq}
                 className="button-faq" 
                 style={buttonColor(props)}>
